424d1740a8295b44d6bf8e2161d544018aa4052f
sahilnagel <sahilnagel@gmail.com>
2026-04-21 11:43:09 +0530

    Added input functionality

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ load_dotenv()
 
 client = genai.Client()
 
-prompt = "Show me 5 recipes for a dish with the following ingredients: chicken, potatoes, and carrots. Per recipe, list all the ingredients used"
+no_recipes = input("No of recipes (for example, 5): ")
+ingredients = input("List of ingredients (for example, chicken, potatoes, and carrots): ")
+prompt = f"Show me {no_recipes} recipes for a dish with the following ingredients: {ingredients}. Per recipe, list all the ingredients used."
 
 response = client.models.generate_content(
     model="gemini-3-flash-preview",
